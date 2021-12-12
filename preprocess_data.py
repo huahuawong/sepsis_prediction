a1ef5d3d17c7e6de0420529df0ffd2fe40c3bf9b
Jun Hua Wong <junhuawong96@gmail.com>
2021-12-12 18:50:21 -0500

    Add files via upload

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,60 +1,123 @@
 import pandas as pd
 import numpy as np
 import os
-
-
-def load_challenge_data(file):
-    with open(file, 'r') as f:
-        header = f.readline().strip()
-        column_names = header.split('|')
-        data = np.loadtxt(f, delimiter='|')
-
-    # Ignore SepsisLabel column if present.
-    if column_names[-1] == 'SepsisLabel':
-        column_names = column_names[:-1]
-        data = data[:, :-1]
-
-    return data
+from tqdm import tqdm
+from utility import save_pickle, compute_prediction_utility, TimeSeriesDataset
+import torch
 
 
 # Specify the file directory where it is located
 input_directory = "./input/"
 
+# File locations for the training dataset
+locations = [input_directory + x for x in ['training_setA/training', 'training_setB/training']]
 
-# Find files.
-files = []
-for f in os.listdir(input_directory):
-    if os.path.isfile(os.path.join(input_directory, f)) and not f.lower().startswith('.') and f.lower().endswith('psv'):
-        files.append(f)
+# Ready to store and concat the two training data files
+data = []
+
+# Make dataframe before saving it into a pkl file. The hospital ids and patient ids are also denoted
+print('Step 1 of 3: Converting to pandas dataframe.')
+
+id = 0
+hospital = 1
+for loc in tqdm(locations):
+    srt_dir = sorted(os.listdir(loc))
+    for file in tqdm(srt_dir):
+        id_df = pd.read_csv(loc + '/' + file, sep='|')
+        id_df['id'] = id  # Give a unique id
+        id_df['hospital'] = hospital  # Note the hospital
+        data.append(id_df)
+        id += 1
+    hospital += 1
+
+# Concat for df
+df = pd.concat(data)
+df.drop('Unnamed: 0', axis=1, errors='ignore', inplace=True)
+
+# Sort index and reorder columns
+df.reset_index(inplace=True)
+df.rename(columns={'index': 'time'}, inplace=True)
+
+# Rearrange the columns of the dataframe
+df = df[['id', 'time'] + [x for x in df.columns if x not in ['id', 'time', 'SepsisLabel']] + ['SepsisLabel']]
+
+save_pickle(df, input_directory + '/raw/df.pickle')
 
 
-num_files = len(files)
-
-for i, f in enumerate(files):
-    print('    {}/{}...'.format(i+1, num_files))
-
-    # Load data.
-    input_file = os.path.join(input_directory, f)
-    data = load_challenge_data(input_file)
-
-#     # Make predictions.
-#     num_rows = len(data)
-#     scores = np.zeros(num_rows)
-#     labels = np.zeros(num_rows)
-#     for t in range(num_rows):
-#         current_data = data[:t+1]
-#         current_score, current_label = get_sepsis_score(current_data, model)
-#         scores[t] = current_score
-#         labels[t] = current_label
-#
-#     # Save results.
-#     output_file = os.path.join(output_directory, f)
-#     save_challenge_predictions(output_file, scores, labels)
+# Convert the scores
+def get_overall_label(df):
+    """ Generates a 1 if the patient developed sepsis at any point. """
+    return df.groupby('id')['SepsisLabel'].apply(max)
 
 
-# # Specify the file name where it is located
-# file_name = "p000007.psv"
-#
-# input_file = file_path + file_name
-# df = pd.read_csv(input_file, sep="|")          # skiprows=skip_rows
+def convert_labels(df):
+    """Convert the binary labels to the corresponding utility score.
+    The labels are given binary 0-1 values, but are scored according to a pre-defined utility score. Here we convert the
+    binary labels onto their corresponding utility value as this value will be more useful for prediction than the
+    binary labels.
+    """
+    def conversion_function(labels):
+        if isinstance(labels, pd.DataFrame):
+            labels = labels['SepsisLabel']
 
+        # Get same length zeros and ones
+        zeros = np.zeros(shape=(len(labels)))
+        ones = np.ones(shape=(len(labels)))
+
+        # Get scores for predicting zero or 1
+        zeros_pred = compute_prediction_utility(labels.values, zeros, return_all_scores=True)
+        ones_pred = compute_prediction_utility(labels.values, ones, return_all_scores=True)
+
+        # Input scores of 0 and 1
+        scores = np.concatenate([zeros_pred.reshape(-1, 1), ones_pred.reshape(-1, 1)], axis=1)
+        scores = pd.DataFrame(index=labels.index, data=scores, columns=[0, 1])
+
+        # Make an overall utilty score equal to one_score - zero_score which encodes the benefit of the 1 prediction
+        scores['utility'] = scores[1] - scores[0]
+
+        return scores
+
+    scores = df.groupby('id').apply(conversion_function)
+
+    return scores
+
+
+print('Step 2 of 3: Converting to labels.')
+overall_labels = torch.Tensor(get_overall_label(df))
+scores = convert_labels(df)
+
+# Some things to save
+binary_labels = torch.Tensor(df['SepsisLabel'].values)
+utility_scores = torch.Tensor(scores['utility'])
+
+
+print('Step 3 of 3: Converting to TimeSeriesDataset format.')
+
+
+def create_timeseries_dataset(df):
+    """ Creates a utility.TimeSeriesDataset from the pandas dataframe. """
+    # Remove unwanted cols
+    df.drop(['time', 'SepsisLabel'], axis=1, inplace=True)
+    columns = list(df.drop(['id'], axis=1).columns)
+
+    # Convert df data to tensor form
+    tensor_data = []
+    ids = df['id'].unique()
+    for id in tqdm(ids):
+        data = df[df['id'] == id].drop('id', axis=1)
+        tensor_data.append(torch.Tensor(data.values.astype(float)))
+
+    # Create dataset
+    dataset = TimeSeriesDataset(data=tensor_data, columns=columns)
+
+    return dataset
+
+
+dataset = create_timeseries_dataset(df)
+
+# Save the data
+save_pickle(scores, input_directory + '/processed/labels/full_scores.pickle')
+save_pickle(utility_scores, input_directory + '/processed/labels/utility_scores.pickle')
+save_pickle(binary_labels, input_directory + '/processed/labels/binary.pickle')
+save_pickle(overall_labels, input_directory + '/processed/labels/overall_labels.pickle')
+dataset.save(input_directory + '/raw/data.tsd')
